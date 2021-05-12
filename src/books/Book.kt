457f27d78c61d842ae8b1ddaf812d934ca979f12
pankaj <pzanzane@gmail.com>
2021-05-12 13:16:09 +0530

    Constants

--- src/books/Book.kt
+++ src/books/Book.kt
@@ -1,6 +1,11 @@
 package books
 
+const val MAXIMUM_BORROW = 10
 open class Book(val title:String, val author:String, val year: Int) {
+
+    companion object {
+        const val BASE_URL = "https//www.library.com/%s.html"
+    }
 
     private var currentPage = 0;
     open fun readPage() = currentPage.inc()
@@ -11,6 +16,12 @@ open class Book(val title:String, val author:String, val year: Int) {
 
     fun titleAuthorYear(): Triple<String, String, Int> {
         return Triple(title, author, year)
+    }
+
+    fun canBorrow(alreadyBorrowed: Int):Boolean = alreadyBorrowed < MAXIMUM_BORROW
+
+    fun printUrl() {
+        println(kotlin.String.format(BASE_URL, title))
     }
 }
 
@@ -23,8 +34,14 @@ class EBook(title:String,
     override fun readPage() = wordCount.plus(250)
 }
 
+object Constats {
+    const val BASE_URL = "https//www.library.com/%s.html"
+}
+
 fun main(arr: Array<String>) {
     val book = Book("Effective Java", "Joshua Bloch", 2005)
     val (title, author, year) = book.titleAuthorYear()
     println("Here is your book $title written by $author in $year")
+
+    book.printUrl()
 }
